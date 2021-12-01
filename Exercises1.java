13f94f9e7d7d75b406e034263331a3d94a4eae57
Alexey <laserjet007@rambler.ru>
2021-12-01 10:19:57 +0300

    Add files via upload
    В приведённом ниже примере используются переменные целого (int) и вещественного (float) типов. Причем переменная first инициируется при объявлении: ей присваивается значение 10.  Затем переменной second присваивается результат выражения, в котором присутствует переменная first. В результате выполнения программы в консоль будут выведены значения обеих переменных: 10 и 7.5 (10/4 + 5).
    Обратите внимание на знак “=” – здесь он означает не равенство, а операцию присвоения.
    Обратите также внимание на цифру 4f в выражении. Так мы указываем что число имеет тип float, чтобы в результате деления также получить float. Можно убрать букву f и оценить как изменится результат.

---
+++ Exercises1.java
@@ -0,0 +1,16 @@
+/**
+ *Java 1. Exercises1
+ *
+ *@author Lazarev Alexei
+ *@version 2021
+ */
+ 
+class Exercises1 {
+    public static void main(String[] args) {
+        int first = 10;
+        float second;
+        second = first / 4f + 5;
+        System.out.println(first);
+        System.out.println(second);
+    }
+}
