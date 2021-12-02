099e26191cd98f3d0fcb56c91a096aa20d86c163
Alexey <laserjet007@rambler.ru>
2021-12-02 14:31:51 +0300

    Add files via upload
    5. Шаг пятый: Игры с циклами
    
    В нелинейных программах также можно использовать циклы. Они предназначены для повторения команд.
    
    Java поддерживает два вида циклов:
    с определенным количеством повторений: for
    без определённого количества повторений: while и do-while
    
    Когда мы спускаемся по лестнице в подъезде, где, как нам известно, ровно 25 ступенек, мы 25 раз повторяем одно и то же действие: “спуститься на ступеньку”. Это пример цикла с определённым количеством повторений.
    
    А когда, например, нам нужно забить в доску гвоздь, то мы повторяем действие “удар молотком” снова и снова, пока не загоним его по самую шляпку. Это пример цикла без определённого количества повторений, который завершается по условию “гвоздь забит”.
    
    class Stairs {
        public static void main(String[] args) {
            for (int i = 0; i < 25; i++) {
                System.out.println("Go down a stair step #" + i);
            }
        }
    }
    
    
    Программа выше иллюстрирует спуск по лестнице из 25 ступенек, которые пронумерованы от 0 до 24, используя цикл for. В круглых скобках находятся три параметра цикла:
    объявление и инициализация счётчика (int i = 0)
    условие  продолжения цикла, логическое выражение (i < 25)
    действие после каждой итерации или выполнения тела цикла (i++) (оператор инкремента ++ равнозначен строке кода i = i + 1;)
    
    Параметры, как видим, разделяются точкой с запятой.
    
    В фигурных скобках находится тело цикла, в нашем случае это одна команда, выводящая текст и номер ступеньки i в консоль. Цикл повторяется в том случае, если логическое выражение во втором параметре возвращает true. После каждого выполнения тела цикла выполняется третий параметр, затем проверяется условие продолжения (второй параметр).

---
+++ Exercises4.java
@@ -0,0 +1,14 @@
+/**
+*Java 1. Exercises4
+*
+*@author Lazarev Alexei
+*@version 2021
+*/
+
+class Exercises4 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 25; i++) {
+            System.out.println("Go down a stair step #" + i);
+        }
+    }
+}
