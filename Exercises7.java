c8a816b20d57477a68d886eacc7dec330610d953
Alexey <laserjet007@rambler.ru>
2021-12-03 11:02:18 +0300

    Add files via upload
    Динамическая инициализация переменных. Начальные значения переменных могут рассчитываться на основе значений других переменных. В примере ниже, для заполнения переменной volume используются значения переменных radius и height.

---
+++ Exercises7.java
@@ -0,0 +1,20 @@
+/**
+*Java 1. Exercises7
+*
+*@author Lazarev Alexei
+*@version 2021
+*/
+
+public class FirstApp {
+    public static void main(String[] args) {
+        float radius = 2.0f, height = 10.0f;
+        // volume инициализируется динамически во время выполнения программы
+        float volume = 3.1416f * radius * radius * height;
+        System.out.println("Объем цилиндра равен " + volume);    
+    }
+}
+
+
+
+
+
