ab1b23da409f42a1b91ca385048a64487d4a16ac
Alexey <laserjet007@rambler.ru>
2021-12-05 14:50:59 +0300

    Add files via upload
    Переменные

---
+++ Exercises9.java
@@ -0,0 +1,14 @@
+/**
+*Java 1. Exercises9
+*
+*@author Lazarev Alexei
+*@version 2021
+*/
+
+public class Exercises9 {
+    public static void main(String[] args) {
+        int a = 20;
+        float b;
+        b = 2.25f;
+    }
+}
