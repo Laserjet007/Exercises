16a4725c576fbb7f7a42bf37f2f34febec309d2e
Alexey <laserjet007@rambler.ru>
2021-12-06 11:44:27 +0300

    Add files via upload
    пример программы, демонстрирующий применение оператора if.

---
+++ Exercises10.java
@@ -0,0 +1,33 @@
+/**
+*Java 1. Exercises10
+*
+*@author Lazarev Alexei
+*@version 2021
+*/
+
+public class Exercises10 {
+    public static void main(String args[]) {
+        int a = 2, b =3, c = 0;
+		if (a < b) {
+			System.out.println("ayukb");
+        }
+        if (a == b) {
+            System.out.println("auykb");
+		}
+        c = a - b;
+        System.out.println("uyu");
+        
+        if (c < 0) {
+			System.out.println("c uyk");
+		}
+        c = b - a;
+        System.out.println("cukyuk");
+
+        if (c >= 0) {
+            System.out.println("cukyuk");
+        }
+        if (c < 0) {
+            System.out.println("cukyuk");
+		}
+	}
+}
