ce7c16d1c95ce124a93b1d4d5b8bbbd9715d326a
Alexey <laserjet007@rambler.ru>
2021-12-09 19:25:14 +0300

    Оператор switch
    Оператор switch позволяет делать выбор между несколькими вариантами дальнейшего выполнения программы. Выражение последовательно сравнивается со списком значений оператора switch. При совпадении выполняется набор операторов, связанных с этим условием. Если совпадений не было, выполняется блок default (блок default является необязательной частью оператора switch). Оператор break останавливает выполнение блока case, если break убрать – выполнение кода продолжится дальше.

---
+++ Exercises21.java
@@ -0,0 +1,19 @@
+/**
+*Java 1. Exercises21
+*
+*@author Lazarev Alexei
+*@version 2021
+*/
+
+public class Exercises21 {
+	switch (выражение) {
+   case значение1:
+      набор_операторов1;
+      break;
+   case значение 2:
+      набор_операторов2;
+      break;
+   ...
+   default:
+      набор_операторов;
+}
