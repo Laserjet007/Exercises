41d5e219d523885da36e295c47f99145dc81365a
Alexey <laserjet007@rambler.ru>
2021-12-13 16:38:47 +0300

    Add files via upload

---
+++ Exercises23.java
@@ -0,0 +1,17 @@
+/**
+*Java 1. Exercises23
+*
+*@author Lazarev Alexei
+*@version 2021
+*/
+
+class Exercises23 {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 5;
+        int c = a+b;
+        if(c == 1){
+            System.out.println("true");
+        }
+    }
+}
